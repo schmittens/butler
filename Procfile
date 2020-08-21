0c4b106333b0e41161845ee1b3eadf821d5173ad
split <christian.schilter@gmail.com>
2020-08-20 21:08:25 -0400

    added procfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn django-butler.wsig:application --log-file - --log-level debug
--- butler/butler/settings.py
+++ butler/butler/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -136,3 +137,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
