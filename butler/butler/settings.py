c40c8cd494f6652c3cff920ce6a430b416b8c653
split <christian.schilter@gmail.com>
2020-08-20 22:25:21 -0400

    added procfile

--- butler/butler/settings.py
+++ butler/butler/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'butler.urls'
+ROOT_URLCONF = 'butler.butler.urls'
 
 TEMPLATES = [
     {
--- butler/butler/urls.py
+++ butler/butler/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sandbox/', include('sandbox.urls'))
+    path('sandbox/', include('butler.sandbox.urls'))
 ]
