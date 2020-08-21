d1e7ef8eb7a84640da22f5d9daf1b72c7fa3009e
split <christian.schilter@gmail.com>
2020-08-20 21:50:02 -0400

    added procfile

--- butler/butler/wsgi.py
+++ butler/butler/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'butler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'butler.butler.settings')
 
 application = get_wsgi_application()
